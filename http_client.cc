cca956cfba0d545bed7a1f8dde39ed353f1522c0
Zach Scheider <zachscheider@gmail.com>
2017-09-21 18:38:07 -0400

    More client work

--- http_client.cc
+++ http_client.cc
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+using namespace std;
+
 #define BUFSIZE 1024
 
 int main(int argc, char * argv[]) {
@@ -69,28 +71,92 @@ int main(int argc, char * argv[]) {
 
 
     /* connect to the server socket */
+    if (minet_connect(socket, &addr) < 0) {
+        fprintf(stderr, "Unable to connect to the server.\n");
+        free(req);
+        exit(-1);
+    }
 
 
     /* send request message */
     sprintf(req, "GET %s HTTP/1.0\r\n\r\n", server_path);
+    if (minet_write(socket, req, !(strlen(req)) >= 0)) {
+        fprintf(stderr, "Unable to send message to server.\n");
+        free(req);
+        exit(-1);
+    }
 
     /* wait till socket can be read. */
     /* Hint: use select(), and ignore timeout for now. */
 
-    /* first read loop -- read headers */
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(socket_fd, &fds);
 
-    /* examine return code */   
+    select(socket + 1, &set, NULL, NULL, NULL);
+
+    /* first read loop -- read headers */
+    char buf[BUFSIZE];
+    int read = minet_read(socket, buf, BUFSIZE - 1);
+    if (read <= 0) {
+       fprintf(stderr, "Unable to read from socket.\n");
+       free(req);
+       close(socket);
+       exit(-1);
+    }
+
+    string response = "";
+    string header = "";
+    size_t pos;
+
+    while (read ) {
+        buf[read] = '\0';
+        response += string(buf);
+        pos = response.find("\r\n\r\n", 0);
+
+        if (pos != string::npos) {
+          header += response.substr(0, pos);
+          response = response.substr(pos + 4);
+          break;
+        } else {
+          header += response;
+        }
+
+        read = minet_read(socket, buf, BUFSIZE - 1);
+    }
+
+    /* examine return code */ 
+    pos = header.find(" ");
+    int ret_code = stoi(header.substr(pos, header.find(" ", pos)));
 
     //Skip "HTTP/1.0"
     //remove the '\0'
 
     // Normal reply has return code 200
-
     /* print first part of response: header, error code, etc. */
+    if (200 == ret_code) {
+        ok = true;
+        cout << response;
+    } else {
+        ok = false;
+        cerr << header + "\r\n\r\n" + response; // Have to use cerr because of strings
+    }
 
     /* second read loop -- print out the rest of the response: real web content */
+    while ((read = minet_read(socket, buf, BUFSIZE - 1)) {
+        buf[read] = '\0';
+        if(ok) {
+            printf("%s", buf);
+        }
+        else {
+            fprintf(stderr, buf);
+        }
+    }
 
     /*close socket and deinitialize */
+    minet_close(socket);
+    minet_deinit();
+    free(req)
 
     if (ok) {
 	return 0;
